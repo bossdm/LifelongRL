ad93838e8da75ade6b7351db2ee077f454fdecd4
bossdm <davidmbossens@gmail.com>
2021-01-10 12:21:09 +0000

    remove default

--- lifelong_cartpole_agent.py
+++ lifelong_cartpole_agent.py
@@ -268,13 +268,13 @@ if __name__ == '__main__':
     parser.add_argument("-x", dest="experiment_type", type=str, default="single")  # single, lifelong_convergence , lifelong
     args = parser.parse_args()
     print("will start run ",args.run, " with experiment_type ",args.experiment_type, "and ",args.policies, " policies of ", args.method)
-    args.VISUAL=False
-    args.method="1to1_DRQN"
-    args.policies=27
-    args.run=5
-    args.experiment_type="lifelong"
-    args.filename="/home/david/LifelongRL"
-    args.environment_file=False
+    # args.VISUAL=False
+    # args.method="1to1_DRQN"
+    # args.policies=27
+    # args.run=5
+    # args.experiment_type="lifelong"
+    # args.filename="/home/david/LifelongRL"
+    # args.environment_file=False
     filename=args.filename +args.experiment_type+str(args.run) + '_' + args.method + str(args.policies) + "pols" + os.environ["tuning_lr"]
     walltime = 60*3600 #60*3600  # 60 hours by default
     if args.walltime:
